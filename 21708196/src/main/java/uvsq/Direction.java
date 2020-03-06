a89b3a4db78fc784e568eddeac5774bae1ff8021
Koussaila HAMMOUCHE <Koussaila.hammouche@ens.uvsq.fr>
2020-03-06 21:16:33 +0100

    Create Direction.java

---
+++ 21708196/src/main/java/uvsq/Direction.java
@@ -0,0 +1,25 @@
+package uvsq;
+
+
+public class Direction {
+	private String directionn; 
+	
+	void EAST() {
+	}
+	void SOUTHEAST() {
+	}
+	void SOUTEAST() {
+	}
+	void SOUTH() {
+	}
+	void SOUTHWEST() {
+	}
+	void WEST() {
+	}
+	void NORTHWEST() {
+	}
+	void NORTH() {
+	}
+	void NORTHEAST() {
+	}
+}
