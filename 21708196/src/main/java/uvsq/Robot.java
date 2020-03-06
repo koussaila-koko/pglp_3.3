9d8ca1b5e4d9f7a4e681899b71b2e423a7ac3c9d
Koussaila HAMMOUCHE <Koussaila.hammouche@ens.uvsq.fr>
2020-03-06 21:16:05 +0100

    Create Robot.java

---
+++ 21708196/src/main/java/uvsq/Robot.java
@@ -0,0 +1,14 @@
+package uvsq;
+
+public class Robot {
+	private Position position;
+	private Direction direction;
+	
+	public void tourne() {}
+
+	public void avance() {
+		//this.position=
+		
+		
+	}
+}
